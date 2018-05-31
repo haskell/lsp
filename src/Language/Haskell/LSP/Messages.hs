1e01dc4e2e764b01e40ed3dbab78ef3c7a41c386
Luke Lau <luke_lau@icloud.com>
2018-05-30 20:09:34 -0400

    Fix message toJSON

--- src/Language/Haskell/LSP/Messages.hs
+++ src/Language/Haskell/LSP/Messages.hs
@@ -56,7 +56,9 @@ data FromClientMessage = ReqInitialized              InitializeRequest
                        | UnknownFromClientMessage        Value
 
 
-  deriving (Eq,Read,Show,Generic,ToJSON,FromJSON)
+  deriving (Eq,Read,Show,Generic,FromJSON)
+instance ToJSON FromClientMessage where
+  toJSON     = genericToJSON $ defaultOptions { sumEncoding = UntaggedValue }
 
 data FromServerMessage = ReqRegisterCapability       RegisterCapabilityRequest
                        | ReqApplyWorkspaceEdit       ApplyWorkspaceEditRequest
@@ -91,4 +93,7 @@ data FromServerMessage = ReqRegisterCapability       RegisterCapabilityRequest
                        | NotShowMessage              ShowMessageNotification
                        -- A cancel request notification is duplex!
                        | NotCancelRequestFromServer  CancelNotification
-  deriving (Eq,Read,Show,Generic,ToJSON,FromJSON)
+  deriving (Eq,Read,Show,Generic,FromJSON)
+
+instance ToJSON FromServerMessage where
+  toJSON     = genericToJSON $ defaultOptions { sumEncoding = UntaggedValue }
