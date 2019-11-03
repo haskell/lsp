dd551b97119ea5fbb0bd1ed4ee16686a5d13b04f
Alan Zimmerman <alan.zimm@gmail.com>
2019-11-03 22:39:02 +0000

    Fix tests compilation

--- test/WorkspaceFoldersSpec.hs
+++ test/WorkspaceFoldersSpec.hs
@@ -25,11 +25,13 @@ spec =
         handlers = def
 
     tvarLspId <- newTVarIO 0
+    mvarVfs <- newMVar (VFSData mempty mempty)
     tvarCtx   <- newTVarIO $ defaultLanguageContextData handlers
                                                         def
                                                         undefined
                                                         tvarLspId
                                                         (const $ return ())
+                                                        mvarVfs
                                                         Nothing
 
     let putMsg msg =
