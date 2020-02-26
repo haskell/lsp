a3d268c9631d2f115111930358b589fcde3aebd8
jneira <atreyu.bbb@gmail.com>
2020-02-26 10:33:24 +0100

    Recover tests over platform aware functions

--- test/URIFilePathSpec.hs
+++ test/URIFilePathSpec.hs
@@ -2,21 +2,22 @@
 {-# LANGUAGE OverloadedStrings #-}
 module URIFilePathSpec where
 
-import Control.Monad (when)
+import Control.Monad                          (when)
 import Data.List
 #if __GLASGOW_HASKELL__ < 808
-import Data.Monoid ((<>))
+import Data.Monoid                            ((<>))
 #endif
 import Data.Text                              (Text, pack)
 import Language.Haskell.LSP.Types
 
-import           Network.URI
+import Network.URI
 import Test.Hspec
 import Test.QuickCheck
 #if !MIN_VERSION_QuickCheck(2,10,0)
 import Data.Char                              (GeneralCategory(..), generalCategory)
 #endif
-import System.FilePath (normalise)
+import qualified System.FilePath.Windows as FPW
+import System.FilePath                        (normalise)
 import qualified System.Info
 -- ---------------------------------------------------------------------
 
@@ -28,9 +29,88 @@ main = hspec spec
 
 spec :: Spec
 spec = do
+  describe "Platform aware URI file path functions" platformAwareUriFilePathSpec
   describe "URI file path functions" uriFilePathSpec
   describe "URI normalization functions" uriNormalizeSpec
   describe "Normalized file path functions" normalizedFilePathSpec
+
+windowsOS :: String
+windowsOS = "mingw32"
+
+testPosixUri :: Uri
+testPosixUri = Uri $ pack "file:///home/myself/example.hs"
+
+testPosixFilePath :: FilePath
+testPosixFilePath = "/home/myself/example.hs"
+
+relativePosixFilePath :: FilePath
+relativePosixFilePath = "myself/example.hs"
+
+testWindowsUri :: Uri
+testWindowsUri = Uri $ pack "file:///c:/Users/myself/example.hs"
+
+testWindowsFilePath :: FilePath
+testWindowsFilePath = "c:\\Users\\myself\\example.hs"
+
+platformAwareUriFilePathSpec :: Spec
+platformAwareUriFilePathSpec = do
+  it "converts a URI to a POSIX file path" $ do
+    let theFilePath = platformAwareUriToFilePath "posix" testPosixUri
+    theFilePath `shouldBe` Just testPosixFilePath
+
+  it "converts a POSIX file path to a URI" $ do
+    let theUri = platformAwareFilePathToUri "posix" testPosixFilePath
+    theUri `shouldBe` testPosixUri
+
+  it "converts a URI to a Windows file path" $ do
+    let theFilePath = platformAwareUriToFilePath windowsOS testWindowsUri
+    theFilePath `shouldBe` Just testWindowsFilePath
+
+  it "converts a Windows file path to a URI" $ do
+    let theUri = platformAwareFilePathToUri windowsOS testWindowsFilePath
+    theUri `shouldBe` testWindowsUri
+
+  it "converts a POSIX file path to a URI" $ do
+    let theFilePath = platformAwareFilePathToUri "posix" "./Functional.hs"
+    theFilePath `shouldBe` (Uri "file://./Functional.hs")
+
+  it "converts a Windows file path to a URI" $ do
+    let theFilePath = platformAwareFilePathToUri windowsOS "./Functional.hs"
+    theFilePath `shouldBe` (Uri "file:///./Functional.hs")
+
+  it "converts a Windows file path to a URI" $ do
+    let theFilePath = platformAwareFilePathToUri windowsOS "c:/Functional.hs"
+    theFilePath `shouldBe` (Uri "file:///c:/Functional.hs")
+
+  it "converts a POSIX file path to a URI and back" $ do
+    let theFilePath = platformAwareFilePathToUri "posix" "./Functional.hs"
+    theFilePath `shouldBe` (Uri "file://./Functional.hs")
+    let Just (URI scheme' auth' path' query' frag') =  parseURI "file://./Functional.hs"
+    (scheme',auth',path',query',frag') `shouldBe`
+      ("file:"
+      ,Just (URIAuth {uriUserInfo = "", uriRegName = ".", uriPort = ""}) -- AZ: Seems odd
+      ,"/Functional.hs"
+      ,""
+      ,"")
+    Just "./Functional.hs" `shouldBe` platformAwareUriToFilePath "posix" theFilePath
+
+  it "converts a Posix file path to a URI and back" $ property $ forAll genPosixFilePath $ \fp -> do
+      let uri = platformAwareFilePathToUri "posix" fp
+      platformAwareUriToFilePath "posix" uri `shouldBe` Just fp
+
+  it "converts a Windows file path to a URI and back" $ property $ forAll genWindowsFilePath $ \fp -> do
+      let uri = platformAwareFilePathToUri windowsOS fp
+      -- We normalise to account for changes in the path separator.
+      -- But driver letters are *not* normalized so we skip them
+      when (not $ "c:" `isPrefixOf` fp) $
+        platformAwareUriToFilePath windowsOS uri `shouldBe` Just (FPW.normalise fp)
+
+  it "converts a relative POSIX file path to a URI and back" $ do
+    let uri = platformAwareFilePathToUri "posix" relativePosixFilePath
+    uri `shouldBe` Uri "file://myself/example.hs"
+    let back = platformAwareUriToFilePath "posix" uri
+    back `shouldBe` Just relativePosixFilePath
+
 
 testUri :: Uri
 testUri | isWindows = Uri "file:///C:/Users/myself/example.hs"
