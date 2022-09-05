6fbe0669f1b79f3623181d1fc89c156f9f65011d
Tom McLaughlin <tom@codedown.io>
2022-09-04 19:04:08 -0700

    lsp-test: export runSessionWithConfig' to allow modifying the CreateProcess (#449)
    * Export runSessionWithConfig' to allow modifying the CreateProcess
    * More meaningful name for runSessionWithConfig'
    * Fix up haddocks
    * Remove stray whitespace

--- lsp-test/src/Language/LSP/Test/Server.hs
+++ lsp-test/src/Language/LSP/Test/Server.hs
@@ -6,13 +6,13 @@ import Language.LSP.Test.Compat
 import System.IO
 import System.Process hiding (withCreateProcess)
 
-withServer :: String -> Bool -> (Handle -> Handle -> ProcessHandle -> IO a) -> IO a
-withServer serverExe logStdErr f = do
+withServer :: String -> Bool -> (CreateProcess -> CreateProcess) -> (Handle -> Handle -> ProcessHandle -> IO a) -> IO a
+withServer serverExe logStdErr modifyCreateProcess f = do
   -- TODO Probably should just change runServer to accept
   -- separate command and arguments
   let cmd:args = words serverExe
       createProc = (proc cmd args) { std_in = CreatePipe, std_out = CreatePipe, std_err = CreatePipe }
-  withCreateProcess createProc $ \(Just serverIn) (Just serverOut) (Just serverErr) serverProc -> do
+  withCreateProcess (modifyCreateProcess createProc) $ \(Just serverIn) (Just serverOut) (Just serverErr) serverProc -> do
     -- Need to continuously consume to stderr else it gets blocked
     -- Can't pass NoStream either to std_err
     hSetBuffering serverErr NoBuffering
