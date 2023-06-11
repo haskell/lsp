fc54bc06bbdd8118ccc43aa867292a3ecbb16172
Nathan Maxson <joyfulmantis@gmail.com>
2023-06-11 22:46:13 +0300

    added WatchKind 7 case to the functions

--- lsp-types/src/Language/LSP/Protocol/Types/WatchKinds.hs
+++ lsp-types/src/Language/LSP/Protocol/Types/WatchKinds.hs
@@ -8,18 +8,21 @@ containsCreate :: WatchKind -> Bool
 containsCreate WatchKind_Create = True
 containsCreate (WatchKind_Custom 3) = True
 containsCreate (WatchKind_Custom 5) = True
+containsCreate (WatchKind_Custom 7) = True
 containsCreate _ = False
 
 containsChange :: WatchKind -> Bool
 containsChange WatchKind_Change = True
 containsChange (WatchKind_Custom 3) = True
 containsChange (WatchKind_Custom 6) = True
+containsChange (WatchKind_Custom 7) = True
 containsChange _ = False
 
 containsDelete :: WatchKind -> Bool
 containsDelete WatchKind_Delete = True
 containsDelete (WatchKind_Custom 5) = True
 containsDelete (WatchKind_Custom 6) = True
+containsDelete (WatchKind_Custom 7) = True
 containsDelete _ = False
 
 combineWatchKinds :: Set WatchKind -> WatchKind
