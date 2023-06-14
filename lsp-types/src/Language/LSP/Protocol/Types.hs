997ba521f2b9c0820f412af8e1fbc356c089351d
Michael Peyton Jones <me@michaelpj.com>
2023-06-14 15:29:31 +0100

    Add codeActionKindSubsumes back

--- lsp-types/src/Language/LSP/Protocol/Types.hs
+++ lsp-types/src/Language/LSP/Protocol/Types.hs
@@ -16,6 +16,8 @@ module Language.LSP.Protocol.Types (
   , module Edits
   -- ** Markup
   , module Markup
+  -- ** Code actions
+  , module CodeAction
   -- ** Progress
   , module Progress
   -- ** Semantic tokens
@@ -24,11 +26,10 @@ module Language.LSP.Protocol.Types (
   , module WatchKinds
   -- * Main LSP types and functions
   , module Generated
-  -- ** Orphan instances for the generated types
-  , module Orphans
   ) where
 
 import Language.LSP.Protocol.Internal.Types as Generated
+import Language.LSP.Protocol.Types.CodeAction as CodeAction
 import Language.LSP.Protocol.Types.Common as Common
 import Language.LSP.Protocol.Types.Location as Locations
 import Language.LSP.Protocol.Types.LspEnum as LspEnum
@@ -39,5 +40,5 @@ import Language.LSP.Protocol.Types.Singletons as Singletons
 import Language.LSP.Protocol.Types.Uri as Uri
 import Language.LSP.Protocol.Types.Uri.OsPath as Uri
 import Language.LSP.Protocol.Types.Edit as Edits
-import Language.LSP.Protocol.Types.Orphans as Orphans
+import Language.LSP.Protocol.Types.Orphans ()
 import Language.LSP.Protocol.Types.WatchKinds as WatchKinds
