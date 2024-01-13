52e9fa64ecc527a27335b487a5f5e6b1663a1c88
Michael Peyton Jones <me@michaelpj.com>
2024-01-13 10:57:03 +0000

    Refactor tests a little

--- lsp-types/test/JsonSpec.hs
+++ lsp-types/test/JsonSpec.hs
@@ -32,37 +32,29 @@ import Test.Hspec.QuickCheck
 import Test.QuickCheck hiding (Success)
 import Test.QuickCheck.Instances ()
 
--- import Debug.Trace
 -- ---------------------------------------------------------------------
 
 {-# ANN module ("HLint: ignore Redundant do" :: String) #-}
 
 spec :: Spec
 spec = do
-  describe "dispatcher" jsonSpec
-  describe "RequestMessage" requestMessageSpec
-  describe "ResponseMessage" responseMessageSpec
-  describe "NotificationMesssage" notificationMessageSpec
+  describe "MarkedString" $ do
+    prop "roundtrip" (propertyJsonRoundtrip :: MarkedString -> Property)
 
--- ---------------------------------------------------------------------
+  describe "MarkupContent" $ do
+    prop "roundtrip" (propertyJsonRoundtrip :: MarkupContent -> Property)
 
-jsonSpec :: Spec
-jsonSpec = do
-  describe "General JSON instances round trip" $ do
-    -- DataTypesJSON
-    prop "MarkedString" (propertyJsonRoundtrip :: MarkedString -> Property)
-    prop "MarkupContent" (propertyJsonRoundtrip :: MarkupContent -> Property)
-    prop "TextDocumentContentChangeEvent" (propertyJsonRoundtrip :: TextDocumentContentChangeEvent -> Property)
-    prop "WatchedFiles" (propertyJsonRoundtrip :: DidChangeWatchedFilesRegistrationOptions -> Property)
+  describe "TextDocumentContentChangeEvent" $ do
+    prop "roundtrip" (propertyJsonRoundtrip :: TextDocumentContentChangeEvent -> Property)
+
+  describe "WatchedFiles" $ do
+    prop "roundtrip" (propertyJsonRoundtrip :: DidChangeWatchedFilesRegistrationOptions -> Property)
+
+  describe "Registration" $ do
     -- Registration has a 'Maybe Value' field, so this test checks that 'Maybe Null' roundtrips properly
-    prop "Registration" (propertyJsonRoundtrip :: Registration -> Property)
-    prop
-      "ResponseMessage Hover"
-      (propertyJsonRoundtrip :: TResponseMessage 'Method_TextDocumentHover -> Property)
-  describe "JSON decoding regressions" $ do
-    it "CompletionItem" $
-      (J.decode "{\"jsonrpc\":\"2.0\",\"result\":[{\"label\":\"raisebox\"}],\"id\":1}" :: Maybe (TResponseMessage 'Method_TextDocumentCompletion))
-        `shouldNotBe` Nothing
+    prop "roundtrip" (propertyJsonRoundtrip :: Registration -> Property)
+
+  describe "CompletionList" $ do
     it "handles optional field set to null in record" $ do
       J.eitherDecode "{ \"isIncomplete\" : true, \"itemDefaults\" : { \"data\" : null }, \"items\": [] }"
         `shouldBe` Right
@@ -72,9 +64,12 @@ jsonSpec = do
               mempty
           )
 
-requestMessageSpec :: Spec
-requestMessageSpec = do
-  describe "edge cases" $ do
+  describe "CompletionItem" $ do
+    it "example" $
+      (J.decode "{\"jsonrpc\":\"2.0\",\"result\":[{\"label\":\"raisebox\"}],\"id\":1}" :: Maybe (TResponseMessage 'Method_TextDocumentCompletion))
+        `shouldNotBe` Nothing
+
+  describe "RequestMessage" $ do
     it "handles missing params field" $ do
       J.eitherDecode "{ \"jsonrpc\": \"2.0\", \"id\": 15, \"method\": \"shutdown\"}"
         `shouldBe` Right (TRequestMessage "2.0" (IdInt 15) SMethod_Shutdown Nothing)
@@ -83,14 +78,10 @@ requestMessageSpec = do
       J.eitherDecode "{ \"jsonrpc\": \"2.0\", \"id\": 15, \"method\": \"shutdown\", \"params\": null }"
         `shouldBe` Right (TRequestMessage "2.0" (IdInt 15) SMethod_Shutdown Nothing)
 
-responseMessageSpec :: Spec
-responseMessageSpec = do
-  describe "edge cases" $ do
-    it "decodes result = null" $ do
-      let input = "{\"jsonrpc\": \"2.0\", \"id\": 123, \"result\": null}"
-       in J.decode input
-            `shouldBe` Just
-              ((TResponseMessage "2.0" (Just (IdInt 123)) (Right $ InL J.Null)) :: TResponseMessage 'Method_WorkspaceExecuteCommand)
+  describe "ResponseMessage" $ do
+    prop
+      "Hover roundtrip"
+      (propertyJsonRoundtrip :: TResponseMessage 'Method_TextDocumentHover -> Property)
     -- The 'data' field on a ResponseError is optional _and_ null is a valid value. Check that we correctly parse null as 'Just Null'
     it "decodes error data = null" $ do
       let input = "{\"jsonrpc\": \"2.0\", \"id\": 123, \"error\": { \"code\": -32700, \"message\": \"oh no\", \"data\": null }}"
@@ -99,7 +90,6 @@ responseMessageSpec = do
               ( (TResponseMessage "2.0" (Just (IdInt 123)) (Left $ ResponseError (InR ErrorCodes_ParseError) "oh no" (Just J.Null))) ::
                   TResponseMessage ('Method_CustomMethod "hello")
               )
-  describe "invalid JSON" $ do
     it "throws if neither result nor error is present" $ do
       (J.eitherDecode "{\"jsonrpc\":\"2.0\",\"id\":1}" :: Either String (TResponseMessage 'Method_Initialize))
         `shouldBe` Left ("Error in $: both error and result cannot be Nothing")
@@ -109,10 +99,13 @@ responseMessageSpec = do
           Either String (TResponseMessage 'Method_Initialize)
         )
         `shouldSatisfy` (either (\err -> "Error in $: both error and result cannot be present" `isPrefixOf` err) (\_ -> False))
+    it "decodes result = null" $ do
+      let input = "{\"jsonrpc\": \"2.0\", \"id\": 123, \"result\": null}"
+       in J.decode input
+            `shouldBe` Just
+              ((TResponseMessage "2.0" (Just (IdInt 123)) (Right $ InL J.Null)) :: TResponseMessage 'Method_WorkspaceExecuteCommand)
 
-notificationMessageSpec :: Spec
-notificationMessageSpec = do
-  describe "edge cases" $ do
+  describe "NotificationMessage" $ do
     it "handles missing params field" $ do
       J.eitherDecode "{ \"jsonrpc\": \"2.0\", \"method\": \"exit\"}"
         `shouldBe` Right (TNotificationMessage "2.0" SMethod_Exit Nothing)
