ca6d2e4ae122288286c8bcc539f8d8b4e9736c94
soulomoon <patrickwalesss@gmail.com>
2025-11-25 18:06:54 +0800

    format

--- lsp/src/Language/LSP/Server/Processing.hs
+++ lsp/src/Language/LSP/Server/Processing.hs
@@ -118,8 +118,8 @@ initializeRequestHandler ::
   ServerDefinition config ->
   VFS ->
   (FromServerMessage -> IO ()) ->
+  -- | Action that waits for the sender thread to finish. We use it to set LanguageContextEnv.resWaitSender, See Note [Shutdown].
   IO () ->
-  -- ^ Action that waits for the sender thread to finish. We use it to set LanguageContextEnv.resWaitSender, See Note [Shutdown].
   TMessage Method_Initialize ->
   IO (Maybe (LanguageContextEnv config))
 initializeRequestHandler logger ServerDefinition{..} vfs sendFunc waitSender req = do
